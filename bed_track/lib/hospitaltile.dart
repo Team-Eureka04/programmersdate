614b61e212478b0ddf7640b14931b1da84a7fa88
Ali Solanki <alisolanki123@gmail.com>
2020-06-27 11:35:21 +0530

    first commit

--- bed_track/lib/hospitaltile.dart
+++ bed_track/lib/hospitaltile.dart
@@ -69,148 +69,149 @@ class _DataTileState extends State<DataTile> {
     return Scaffold(
       body: Center(
         child: Container(
-            padding: EdgeInsets.all(0),
-            margin: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-            height: 0.9 * height,
-            width: 0.9 * width,
-            decoration: BoxDecoration(
-              color: Colors.grey[850],
-              borderRadius: BorderRadius.circular(20),
-              boxShadow: [
-                BoxShadow(
-                  blurRadius: 15.0,
-                  offset: Offset(4.0, 4.0),
-                  color: Colors.black,
-                  spreadRadius: 1.0,
-                ),
-              ],
-            ),
-            child: Container(
-              child: Expanded(
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Container(
-                      child: SingleChildScrollView(
-                        child: Column(
-                          children: <Widget>[
-                            Hero(
-                              tag: hospitalName,
-                              child: Container(
-                                height: height * 0.25,
-                                width: double.infinity,
-                                decoration: BoxDecoration(
-                                    image: DecorationImage(
-                                      image: NetworkImage(imageURL),
-                                      fit: BoxFit.cover,
-                                    ),
-                                    borderRadius: BorderRadius.circular(10)),
-                              ),
-                            ),
-                            Container(
-                              margin: EdgeInsets.all(10),
-                              child: Text(
-                                hospitalName,
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 30,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                                softWrap: true,
-                              ),
-                            ),
-                            SizedBox(
-                              width: 20,
-                            ),
-                            Container(
-                              margin: EdgeInsets.symmetric(
-                                  vertical: 10, horizontal: 10),
-                              child: Text(
-                                "Beds available = ${bedNumber.toString()}",
-                                style: TextStyle(
-                                  color: bedNumber == 0
-                                      ? Colors.red
-                                      : bedNumber <= 10
-                                          ? Colors.yellow
-                                          : Colors.green,
-                                  fontSize: 20,
-                                ),
-                              ),
-                            ),
-                            Container(
-                              margin: EdgeInsets.symmetric(
-                                  vertical: 10, horizontal: 10),
-                              child: Text(
-                                "${location.address}",
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 20),
-                              ),
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Icon(
-                                  Icons.phone,
-                                  color: Colors.white,
-                                  size: 20,
-                                ),
-                                Container(
-                                  margin: EdgeInsets.symmetric(
-                                      vertical: 10, horizontal: 10),
-                                  child: Text(
-                                    phoneNumber.toString(),
-                                    style: TextStyle(
-                                        color: Colors.white, fontSize: 20),
+          padding: EdgeInsets.all(0),
+          margin: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+          height: 0.9 * height,
+          width: 0.9 * width,
+          decoration: BoxDecoration(
+            color: Colors.grey[850],
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 15.0,
+                offset: Offset(4.0, 4.0),
+                color: Colors.black,
+                spreadRadius: 1.0,
+              ),
+            ],
+          ),
+          child: Container(
+            child: Expanded(
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Container(
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: <Widget>[
+                          Hero(
+                            tag: hospitalName,
+                            child: Container(
+                              height: height * 0.25,
+                              width: double.infinity,
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                    image: NetworkImage(imageURL),
+                                    fit: BoxFit.cover,
                                   ),
-                                ),
-                              ],
+                                  borderRadius: BorderRadius.circular(10)),
                             ),
-                            SizedBox(
-                              height: 10,
+                          ),
+                          Container(
+                            margin: EdgeInsets.all(10),
+                            child: Text(
+                              hospitalName,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 30,
+                                fontWeight: FontWeight.w500,
+                              ),
+                              softWrap: true,
                             ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Text(
-                                  "${distance == null ? 0 : distance.ceil() / 1000}",
+                          ),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Container(
+                            margin: EdgeInsets.symmetric(
+                                vertical: 10, horizontal: 10),
+                            child: Text(
+                              "Beds available = ${bedNumber.toString()}",
+                              style: TextStyle(
+                                color: bedNumber == 0
+                                    ? Colors.red
+                                    : bedNumber <= 10
+                                        ? Colors.yellow
+                                        : Colors.green,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.symmetric(
+                                vertical: 10, horizontal: 10),
+                            child: Text(
+                              "${location.address}",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 20),
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Icon(
+                                Icons.phone,
+                                color: Colors.white,
+                                size: 20,
+                              ),
+                              Container(
+                                margin: EdgeInsets.symmetric(
+                                    vertical: 10, horizontal: 10),
+                                child: Text(
+                                  phoneNumber.toString(),
                                   style: TextStyle(
                                       color: Colors.white, fontSize: 20),
                                 ),
-                                SizedBox(
-                                  width: 10,
-                                ),
-                                Icon(
-                                  Icons.location_on,
-                                  color: Colors.red.withOpacity(0.6),
-                                  size: 20,
-                                )
-                              ],
-                            ),
-                          ],
-                        ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text(
+                                "${distance == null ? 0 : distance.ceil() / 1000}",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 20),
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Icon(
+                                Icons.location_on,
+                                color: Colors.red.withOpacity(0.6),
+                                size: 20,
+                              )
+                            ],
+                          ),
+                        ],
                       ),
                     ),
-                    Container(
-                      margin: EdgeInsets.all(20),
-                      child: FloatingActionButton(
-                        elevation: 0,
-                        highlightElevation: 0,
-                        backgroundColor: Colors.grey,
-                        child: Icon(
-                          Icons.arrow_back_ios,
-                          color: Colors.black,
-                        ),
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(20),
+                    child: FloatingActionButton(
+                      elevation: 0,
+                      highlightElevation: 0,
+                      backgroundColor: Colors.grey,
+                      child: Icon(
+                        Icons.arrow_back_ios,
+                        color: Colors.black,
                       ),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            )),
+            ),
+          ),
+        ),
       ),
     );
   }
